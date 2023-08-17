6574505bcd29bfe4e1fcecbe7dd9a0c84db0993b
Alejandro Berrini <alejandroberrini@hotmail.com>
2023-08-16 21:07:46 -0300

    Feat/#1 Ecomerce primer comit

---
+++ ecomerce2022/client/js/products.js
@@ -0,0 +1,92 @@
+const productos = [
+    {
+        id:1,
+        productName:"Cafe",
+        price:"$600",
+        quantity:1,
+        img:"/client/media/cafe.jpg",
+
+    },
+
+    {
+        id:2,
+        productName:"Cafe con Leche",
+        price:"$700",
+        quantity:1,
+        img:"/client/media/cafeconLeche.jpg",
+        
+    },
+
+    {
+        id:3,
+        productName:"Cookies",
+        price:"$400",
+        quantity:1,
+        img:"/client/media/cookies.jpg",
+        
+    },
+
+    {
+        id:4,
+        productName:"Medias Lunas",
+        price:"$300",
+        quantity:1,
+        img:"/client/media/mediaslunas.jpg",
+        
+    },
+
+    {
+        id:5,
+        productName:"Rolls de Canela",
+        price:"$500",
+        quantity:1,
+        img:"/client/media/rolloscanela.jpg",
+        
+    },
+
+    {
+        id:6,
+        productName:"Tarta de Frutos",
+        price:"$900",
+        quantity:1,
+        img:"/client/media/tartaFrutos.jpg",
+        
+    },
+
+    {
+        id:7,
+        productName:"Te",
+        price:"$700",
+        quantity:1,
+        img:"/client/media/te.jpg",
+        
+    },
+
+    {
+        id:8,
+        productName:"Porción de Torta",
+        price:"$1500",
+        quantity:1,
+        img:"/client/media/torta.jpg",
+        
+    },
+
+    {
+        id:9,
+        productName:"Tostadas Con Dulce",
+        price:"$500",
+        quantity:1,
+        img:"/client/media/tostadas.jpg",
+        
+    },
+
+    {
+        id:10,
+        productName:"Tostado",
+        price:"$1500",
+        quantity:1,
+        img:"/client/media/tostado.jpg",
+        
+    },
+
+];
