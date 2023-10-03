c3ad69b9f8e3b019cbfb2a2dab2abe81178e41de
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-10-03 12:01:31 -0300

    -feat: ajusto todo el codigo anterior para que este bien formateado

---
+++ PERN-stack-clase7/src/controllers/auth.controller.js
@@ -0,0 +1,7 @@
+export const singin = (req, res) => res.send("Ingresando")
+
+export const singup = (req, res) => res.send("Registrando")
+
+export const singout = (req, res) => res.send("Cerrando sesion")
+
+export const profile = (req, res) => res.send("Perfil de usuario")
